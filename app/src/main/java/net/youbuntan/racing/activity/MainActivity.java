dc7bf651b0822a2ce99fd287a01bd38847bc9adc
加瀬 賢樹 <KaseMasaki@next-group.jp>
2014-09-11 18:20:59 +0900

    Update setting for first commit

--- app/src/main/java/net/youbuntan/racing/activity/MainActivity.java
+++ app/src/main/java/net/youbuntan/racing/activity/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import kasemas.myapplication.R;
+import net.youbuntan.racing.R;
 
 
 public class MainActivity extends Activity {
