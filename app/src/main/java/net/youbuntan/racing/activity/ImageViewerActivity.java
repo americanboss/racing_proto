055d2f45e686d0e4756ac85f46c9cde640deb51b
加瀬 賢樹 <KaseMasaki@next-group.jp>
2015-03-03 18:45:20 +0900

    ScalableImageView add

--- app/src/main/java/net/youbuntan/racing/activity/ImageViewerActivity.java
+++ app/src/main/java/net/youbuntan/racing/activity/ImageViewerActivity.java
@@ -1,64 +1,38 @@
 package net.youbuntan.racing.activity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.FrameLayout;
-import android.widget.HorizontalScrollView;
-import android.widget.TextView;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import net.youbuntan.racing.R;
-import net.youbuntan.racing.adapter.RaceViewerPagerAdapter;
-import net.youbuntan.racing.logic.AssetsLogic;
-import net.youbuntan.racing.model.RaceList;
-
-import java.lang.reflect.Type;
-
+import net.youbuntan.racing.util.ScalableImageView;
 
 public class ImageViewerActivity extends FragmentActivity {
+
+    private ScalableImageView mScaleImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_image_view);
 
         FrameLayout frameLayout = (FrameLayout) findViewById(R.id.viewer);
 
-        frameLayout.addView(new ScaleImageView(this));
+        mScaleImageView = new ScalableImageView(this);
+        mScaleImageView.setMaxScale(4);
+        mScaleImageView.setDrawable(R.drawable.sample_1);
+        frameLayout.addView(mScaleImageView);
 
 
     }
 
-    class ScaleImageView extends View {
-
-        private Bitmap mBitmap;
-
-        public ScaleImageView(Context context) {
-            super(context);
-
-            mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample_1);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-
-            canvas.drawBitmap(mBitmap, 0, 0, null);
-        }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
 }
---
+++ app/src/main/java/net/youbuntan/racing/util/ScalableImageView.java
@@ -0,0 +1,331 @@
+package net.youbuntan.racing.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import net.youbuntan.racing.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * すけーらぶるなImageView
+ */
+public class ScalableImageView  extends View implements View.OnTouchListener, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+    private Bitmap mBitmap;
+
+    private Controller mController;
+
+    private static final String TAG = "ScaleImageView";
+
+    private float mX;
+    private float mY;
+    private float mScale;
+
+    private int mMaxScale;
+
+    private int mScreenWidth;
+    private int mScreenHeight;
+    private int mImageWidth;
+    private int mImageHeight;
+    private float mBaseScale;
+
+    private GestureDetector mGestureDetector;
+
+    private Timer mTimer;
+
+    @Override
+    protected void onLayout(final boolean changed, final int left, final int top, final int right, final int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        mScreenWidth = this.getWidth();
+        mScreenHeight = this.getHeight();
+
+        float scaleW = (float) mScreenWidth / (float) mImageWidth;
+        float scaleH = (float) mScreenHeight / (float) mImageHeight;
+        mScale = (scaleH < scaleW) ? scaleH : scaleW;
+        mBaseScale = mScale;
+
+        mTimer = new Timer(false);
+
+        setValidParam();
+        invalidate();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        if (mTimer != null) {
+            mTimer.cancel();
+        }
+        super.onDetachedFromWindow();
+    }
+
+    public ScalableImageView(Context context) {
+        super(context);
+
+        setOnTouchListener(this);
+
+        mController = new Controller();
+
+        mX = 0;
+        mY = 0;
+        mScale = mBaseScale;
+        mMaxScale = 3;
+
+        mGestureDetector = new GestureDetector(context, this);
+
+        mTimer = new Timer(false);
+    }
+
+    public void setMaxScale(final int maxScale) {
+        mMaxScale = maxScale;
+    }
+
+    public void setDrawable(final int drawable) {
+        mBitmap = BitmapFactory.decodeResource(getResources(), drawable);
+        mImageWidth = mBitmap.getWidth();
+        mImageHeight = mBitmap.getHeight();
+    }
+
+    private float getXByCenter(final float centerX) {
+        return centerX + (mScreenWidth / (2 * mScale));
+    }
+    private float getYByCenter(final float centerY) {
+        return centerY + (mScreenHeight / (2 * mScale));
+    }
+
+    @Override
+    public boolean onTouch(final View v, final MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            mController.lastX = event.getX();
+            mController.lastY = event.getY();
+            mController.touched = true;
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (mController.touched) {
+                moved(event.getX(), event.getY());
+                mController.touched = false;
+            }
+        } else {
+            if (mController.touched) {
+                moved(event.getX(), event.getY());
+            }
+        }
+        mGestureDetector.onTouchEvent(event);
+        return true;
+    }
+
+    private void moved(final float distX, final float distY) {
+        float moveX = (distX - mController.lastX) / mScale;
+        float moveY = (distY - mController.lastY) / mScale;
+        mController.lastX = distX;
+        mController.lastY = distY;
+
+        mX += moveX;
+        mY += moveY;
+
+        setValidParam();
+
+        invalidate();
+    }
+
+    private float getNextScale() {
+        float scale = mScale;
+        if (scale == mBaseScale * mMaxScale) {
+            scale = mBaseScale;
+        } else if (scale + mBaseScale > mBaseScale * mMaxScale) {
+            scale = mMaxScale;
+        } else {
+            scale = scale + mBaseScale;
+        }
+        return scale;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.scale(mScale, mScale);
+        canvas.drawBitmap(mBitmap, mX, mY, null);
+    }
+
+    @Override
+    public boolean onDown(final MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(final MotionEvent e) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(final MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(final MotionEvent e1, final MotionEvent e2, final float distanceX, final float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(final MotionEvent e) {
+    }
+
+    @Override
+    public boolean onFling(final MotionEvent e1, final MotionEvent e2, final float velocityX, final float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(final MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(final MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(final MotionEvent e) {
+        if (e.getAction() == MotionEvent.ACTION_UP) {
+            // 移動量を計算
+            if (mScale > getNextScale()) {
+                startResetAnimation();
+            } else {
+                float moveX = ((mScreenWidth / 2) - e.getX()) / mScale;
+                float moveY = ((mScreenHeight / 2) - e.getY()) / mScale;
+                startZoomAnimation(moveX, moveY, getNextScale());
+            }
+        }
+        return false;
+    }
+
+    private void startZoomAnimation(final float moveX, final float moveY, final float nextScale) {
+        if (mCount == 0) {
+            mCount = COUNT_DOWN;
+            mTimer.schedule(new AnimationTimer(moveX, moveY, nextScale), 0, 200 / COUNT_DOWN);
+        }
+    }
+
+    private void startResetAnimation() {
+        if (mCount == 0) {
+            mCount = COUNT_DOWN;
+            mTimer.schedule(new AnimationTimer(), 0, 200 / COUNT_DOWN);
+        }
+    }
+
+    private void setValidParam() {
+        // 横位置
+        if (mImageWidth * mScale < mScreenWidth) {
+            mX = getXByCenter(-(mImageWidth / 2));
+        } else if (mX > 0) {
+            mX = 0;
+        } else if (mX - (mScreenWidth / mScale) < 0 - mImageWidth) {
+            mX = ( - mImageWidth + (mScreenWidth / mScale));
+        }
+
+        // 縦位置
+        if (mImageHeight * mScale < mScreenHeight) {
+            mY = getYByCenter(-(mImageHeight / 2));
+        } else if (mY > 0) {
+            mY = 0;
+        } else if (mY - (mScreenHeight / mScale) < 0 - mImageHeight) {
+            mY = ( - mImageHeight + (mScreenHeight / mScale));
+        }
+
+    }
+
+
+    private int mCount;
+    private static final int COUNT_DOWN = 60;
+
+    class AnimationTimer extends TimerTask {
+        private float startX;
+        private float startY;
+        private float startCenterX;
+        private float startCenterY;
+        private float startScale;
+        private float endX;
+        private float endY;
+        private float endCenterX;
+        private float endCenterY;
+        private float endScale;
+
+        AnimationTimer(final float moveX, final float moveY, final float endScale) {
+            startX = mX;
+            startY = mY;
+            startCenterX = startX - (mScreenWidth / (2 * mScale ) );
+            startCenterY = startY - (mScreenHeight / (2 * mScale ) );
+            startScale = mScale;
+            endX = mX + moveX / endScale;
+            endY = mY + moveY / endScale;
+            endCenterX = endX - (mScreenWidth / (2 * endScale ) );
+            endCenterY = endY - (mScreenHeight / (2 * endScale ) );
+            this.endScale = endScale;
+
+        }
+
+        AnimationTimer() {
+            startX = mX;
+            startY = mY;
+            startCenterX = startX - (mScreenWidth / (2 * mScale ) );
+            startCenterY = startY - (mScreenHeight / (2 * mScale ) );
+            startScale = mScale;
+            endX = 0;
+            endY = 0;
+            endScale = mBaseScale;
+            endCenterX = endX - (mScreenWidth / (2 * endScale ) );
+            endCenterY = endY - (mScreenHeight / (2 * endScale ) );
+        }
+
+        private float getStep() {
+            return (float) 1 / COUNT_DOWN * (COUNT_DOWN - mCount);
+        }
+
+        @Override
+        public void run() {
+            mCount--;
+            float centerX = startCenterX + (endCenterX - startCenterX) * getStep();
+            float centerY = startCenterY + (endCenterY - startCenterY) * getStep();
+            float scale = startScale + (endScale - startScale) * getStep();
+
+            mX = getXByCenter(centerX);
+            mY = getYByCenter(centerY);
+            mScale = scale;
+
+            setValidParam();
+
+            mHandler.post(mRunnable);
+
+            if (mCount == 0) {
+                this.cancel();
+            }
+        }
+    }
+
+    Handler mHandler = new Handler();
+    Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            invalidate();
+        }
+    };
+
+    class Controller {
+        private float lastX;
+        private float lastY;
+        private boolean touched;
+
+        Controller() {
+            lastX = 0;
+            lastY = 0;
+            touched = false;
+        }
+    }
+}
