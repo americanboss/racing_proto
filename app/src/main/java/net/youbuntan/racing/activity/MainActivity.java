11657baad53030e53e9fe3c24fb35e5e7596ce6a
Masaki Kase <masaki.kase@equinestudio.co.jp>
2015-11-07 00:16:12 +0900

    transform to drawer style.

--- app/src/main/java/net/youbuntan/racing/activity/MainActivity.java
+++ app/src/main/java/net/youbuntan/racing/activity/MainActivity.java
@@ -1,29 +1,114 @@
 package net.youbuntan.racing.activity;
 
-import android.app.Activity;
-import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import net.youbuntan.racing.R;
+import net.youbuntan.racing.adapter.DrawerMenuListAdapter;
+import net.youbuntan.racing.fragment.HomeFragment;
+import net.youbuntan.racing.model.DrawerMenuItem;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity {
 
     private LinearLayout mContainer;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawer;
+
+    private LinearLayout mLeftDrawer;
+    private LinearLayout mDrawerHeadContainer;
+
+    private ListView mDrawerMenuList;
+    private DrawerMenuListAdapter mDrawerMenuListAdapter;
+    List<DrawerMenuItem> mDrawerMenuItems;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mContainer = (LinearLayout) findViewById(R.id.container);
+        mLeftDrawer = (LinearLayout) findViewById(R.id.left_drawer);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerMenuList = (ListView) findViewById(R.id.eqs_drawer_menu_list);
+        mDrawerHeadContainer = (LinearLayout) findViewById(R.id.eqs_drawer_head_container);
 
-        FragmentManager fragmentManager = getFragmentManager();
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the navigation drawer and the action bar app icon.
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                             /* host Activity */
+                mDrawer,                    /* DrawerLayout object */
+                R.string.app_name,  /* "open drawer" description for accessibility */
+                R.string.dialog_cancel  /* "close drawer" description for accessibility */
+        ) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                invalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+            }
+        };
+
+        // Defer code dependent on restoration of previous instance state.
+        // NB: required for the drawer indicator to show up!
+        mDrawer.setDrawerListener(mDrawerToggle);
+
+        mDrawerMenuListAdapter = new DrawerMenuListAdapter(this);
+        mDrawerMenuList.setAdapter(mDrawerMenuListAdapter);
+        mDrawerMenuList.setOnItemClickListener(mDrawerMenuItemClickListener);
+
+        mDrawerMenuItems = new ArrayList<>();
+
+        replaceInformationFragment();
 
     }
 
+    private AdapterView.OnItemClickListener mDrawerMenuItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
+            switch (mDrawerMenuItems.get(position).getId()) {
+//                case DrawerMenuItem.ID_MAP:
+//                    replaceLocationMapFragment(LocationMapFragment.MOVE_TO_PLACE_NONE);
+//                    break;
+            }
 
+        }
+    };
+
+    protected void replaceInformationFragment () {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        HomeFragment informationFragment = HomeFragment.getNewInstance();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content_container, informationFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        if (mDrawer.isDrawerOpen(mLeftDrawer)) {
+            mDrawer.closeDrawer(mLeftDrawer);
+        }
+    }
+
+    private void openDrawer() {
+        if (!mDrawer.isDrawerOpen(mLeftDrawer)) {
+            mDrawer.openDrawer(mLeftDrawer);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
